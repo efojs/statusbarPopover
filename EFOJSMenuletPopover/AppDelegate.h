e002da4a66fdc98e110164c60f331482f9469c32
playinRbt <prosto-c@ya.ru>
2015-03-11 00:28:31 +0300

    Initial Commit

---
+++ EFOJSMenuletPopover/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EFOJSMenuletPopover
+//
+//  Created by prosto on 11/03/15.
+//  Copyright (c) 2015 Denis Krasulin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ EFOJSMenuletPopover/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  EFOJSMenuletPopover
+//
+//  Created by prosto on 11/03/15.
+//  Copyright (c) 2015 Denis Krasulin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
